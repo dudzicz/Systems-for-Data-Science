534647df5ec4c3b9c15eb14677f751f2c877ddb7
Vincent Coriou <vincent.coriou@gamil.com>
2019-03-25 16:20:25 +0100

    Add main file

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,63 @@
+import java.lang.Integer.max
+
+import org.apache.spark.{SparkConf, SparkContext}
+import svm.SVM.svm
+
+class Main {
+  def main(args: Array[String]): Unit = {
+    val appName = "g1"
+    val master = "local"
+
+    val conf = new SparkConf().setAppName(appName).setMaster(master)
+    val sc = new SparkContext(conf)
+
+    val datalines = sc.textFile("hdfs://data/datasets/lyrl2004_vectors_train.dat")
+    val topics = sc.textFile("hdfs://data/datasets/rcv1-v2.topics.qrels")
+
+
+    val selected = "CCAT"
+
+    val labels = topics.map(i => {
+      val p = i.split("")
+      p(1).toInt -> p(0)
+    }).groupByKey().mapValues(i => {
+      if (i.toList.contains(selected)) 1 else -1
+    })
+
+
+    val data = datalines.map(i => {
+      val q = i.split("  ")
+      val l = q(0).toInt
+      val p = q(1).split(" ")
+      val x = p.map(j => {
+        val p = j.split(":")
+        p(0).toInt -> p(1).toDouble
+      }).toMap
+      (l, x)
+    })
+
+    val full_data = data.join(labels)
+    val dimensions: Int = data.map(x => x._2.keys.max).reduce(max)
+
+    val epochs = 10
+    val batchSize = 128
+    val gamma = 0.01
+
+    var weights: List[Double] = List.fill(dimensions)(0.0)
+
+    for (e <- 0 to epochs) {
+      val g = full_data.map(p => {
+        val x = p._2._1
+        val y = p._2._2
+        svm(x, y, weights).toSeq
+      }).reduce((p, q) => {
+        p ++ q
+      }).groupBy(_._1).mapValues(p => p.map(_._2).sum)
+      for ((a, b) <- g) {
+        weights(a) -= gamma * b
+      }
+    }
+
+
+  }
+}
--- src/main/scala/svm/SVM.scala
+++ src/main/scala/svm/SVM.scala
@@ -8,6 +8,10 @@ object SVM {
     max(a, 0)
   }
 
+  def scalar_product(a: List[Double], b: List[Double]): Double = {
+    a zip b map (x => x._1 * x._2) sum
+  }
+
   def svm(x: Map[Int, Double], y: Int, w: List[Double]): Map[Int, Double] = {
     val xe = x.values.toList
     val we = select(x, w)
@@ -15,25 +19,20 @@ object SVM {
     x.keys.zip(g).toMap
   }
 
-  def scalar_product(a: List[Double], b: List[Double]): Double = {
-    a zip b map (x => x._1 * x._2) sum
-  }
-
-
-  def add(a: List[Double], b: List[Double]): List[Double] = {
-    a zip b map (x => x._1 + x._2)
+  def gradient(x: List[Double], y: Int, w: List[Double]): List[Double] = {
+    val a = y * scalar_product(w, x)
+    if (a >= 1) x map (_ => 0.0) else scalar_mult(x, -y)
   }
 
   def scalar_mult(a: List[Double], b: Double): List[Double] = {
     a map (_ * b)
   }
 
-  def gradient(x: List[Double], y: Int, w: List[Double]): List[Double] = {
-    val a = y * scalar_product(w, x)
-    if (a >= 1) x map (_ => 0.0) else scalar_mult(x, -y)
-  }
-
   def select(x: Map[Int, Double], w: List[Double]): List[Double] = {
     x.keys.toList map w
+  }
+
+  def add(a: List[Double], b: List[Double]): List[Double] = {
+    a zip b map (x => x._1 + x._2)
   }
 }
